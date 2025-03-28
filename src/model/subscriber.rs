629ff544c911b1c0aa77bbb02452385828b88689
Danniel-Ang <danniel@ui.ac.id>
2025-03-28 16:59:51 +0700

    [Fix] Fixing Error Found in Log

--- src/model/subscriber.rs
+++ src/model/subscriber.rs
@@ -1,9 +1,11 @@
 use rocket::serde::{Deserialize, Serialize};
-use rocket::log;
 use rocket::serde::json::to_string;
 use rocket::tokio;
 use bambangshop::REQWEST_CLIENT;
 use crate::model::notification::Notification;
+
+use rocket::serde::json::serde_json; 
+use log::{warn};  
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
@@ -18,12 +20,13 @@ impl Subscriber {
         REQWEST_CLIENT
             .post(&self.url)
             .header("Content-Type", "application/json")
-            .body(serde_json::to_string(&payload).unwrap())
+            .body(serde_json::to_string(&payload).unwrap()) // Gunakan serde_json langsung
             .send()
             .await
             .ok();
         
-        log::warn!("Sent {} notification of: [{}] {}, to: {}",
+        warn!(
+            "Sent {} notification of: [{}] {}, to: {}",
             payload.status, payload.product_type, payload.product_title, self.url
         );
     }
