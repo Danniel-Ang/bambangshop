0696dc003e6fa4b2da52458f44c1bcdbf193127b
Danniel-Ang <danniel@ui.ac.id>
2025-03-28 16:44:54 +0700

    Implement publish function in Program service and Program controller.

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -4,9 +4,8 @@ use rocket::fairing::AdHoc;
 
 pub fn route_stage() -> AdHoc {
     return AdHoc::on_ignite("Initializing controller routes...", |rocket| async {
-        rocket
-            .mount("/product", routes![product::create, product::list, product::read, product::delete])
-            .mount("/notification", routes![notification::subscribe, notification::unsubscribe]) // Tambahkan handler subscribe
+        rocket.mount("/product", routes![product::create, product::list, product::read, product::delete, product::publish])
+            .mount("/notification", routes![notification::subscribe, notification::unsubscribe]) 
     });
 }
 
